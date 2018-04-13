5b4bf4fb4bcf63f181b725448d17853ee68f1a3d
Исаев Алексей Витальевич <anxieter@yandex.ru>
2018-04-13 18:37:21 +0300

    homework_4 in process 3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import Switcher from '../Switcher';
-
 import './App.css';
 
 class App extends Component {
+
+  state = { selectedChild: 0 };
+
+  handleChangeChild = (event) => {
+    let dataProp = event.target.getAttribute('data-id');
+    this.setState({ selectedChild: dataProp });
+  }
+
   render() {
     return (
-      <Switcher />
+      <div className="switcher">
+        <nav>
+          <ul className="component-list">
+            <li className="component-list__name" data-id="0" onClick={this.handleChangeChild} >VideoPlayer</li>
+            <li className="component-list__name" data-id="1" onClick={this.handleChangeChild} >Card number formating</li>
+            <li className="component-list__name" data-id="2" onClick={this.handleChangeChild} >ModalButton</li>
+          </ul >
+        </nav >
+        <hr></hr>
+        <Switcher value={this.state.selectedChild} />
+      </div >
+
     );
   }
 }
--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -1,99 +1,32 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import VideoPlayer from '../VideoPlayer';
 import ModalButton from '../ModalButton';
-// import CardNumberHolder from '../CardNumberHolder'; <CardNumberHolder />
+import CardNumberHolder from '../CardNumberHolder';
 import './Switcher.css';
 
 // Для работы этой компоненты нужно использовать методы React.Children.toArray
 // а так же работать с child.type.name и child.type.displayName
 
-const { Provider, Consumer } = React.createContext(0);
-export class Switcher extends Component {
 
-  state = { selectedChild: 0 };
-
-  handleChangeChild = () => { // переключение по ссылкам меняет  state
-    this.setState(state => {
-      return state.selectedChild === 0
-        ? { [data - id]: 0 }
-        : { [data - id]: 1 };
-    });
-  }
+class Switcher extends PureComponent {
 
   render() {
-    return (
-      <Provider value={this.state.selectedChild}>
-        <IntermediateComponent>
-          <ThemedButton />
-        </IntermediateComponent>
-      </Provider>
-    );
-
-    //   <div className="switcher">
-    //   <nav>
-    //     <ul className="component-list">
-    //       <li className="component-list__name" data-id={0} onClick={this.handleChangeChild} >VideoPlayer</li>
-    //       <li className="component-list__name" data-id={1} onClick={this.handleChangeChild} >Card number formating</li>
-    //       <li className="component-list__name" data-id={2} onClick={this.handleChangeChild} >ModalButton</li>
-    //     </ul>
-    //   </nav>
-    //   <hr></hr>
-    //   <div className="component-wrapper">
-    //       <VideoPlayer />
-    //       <br />
-    //       <ModalButton />
-    //   </div>
-    // </div>
-
+    const block = this.props.value;
+    return this.props.render({ block });
   }
+
+  // render() {
+  // let componentBlock = null;
+  // console.log(this.props.value);
+  // if (this.props.value === 0) { componentBlock = < VideoPlayer /> }
+  // else if (this.props.value === 1) { componentBlock = <ModalButton /> }
+  // else if (this.props.value === 2) { componentBlock = <CardNumberHolder /> }
+  // return (<div className="component-wrapper">{componentBlock}</div>);
+  //   }
 }
 
-const IntermediateComponent = ({ children }) => {
-  return <div>{children}</div>;
-};
+// export default Switcher;
 
-function exactComponent(WrappedComponent) {
-  return class extends Component {
-    render() {
-      return (
-        <Consumer>
-          {theme => (
-            <WrappedComponent
-              {...this.props}
-              theme={theme}
-            />
-          )}
-        </Consumer>
-      );
-    }
-  };
+export default () => {
+  <Switcher render={({ block }) => <p>{block}</p>}></Switcher >
 }
-
-const Button = ({ theme }) => (
-  <button
-    style={{
-      backgroundColor: theme === 'light' ? '#333' : '#eee',
-    }}
-  >
-    Кнопка с темой
-  </button>
-);
-
-const ThemedButton = exactComponent(Button);
-
-
-
-// class Switcher extends Component {
-
-//   state = {
-//     selectedChild: 0
-//   }
-
-//   render() {
-//     return (
-//       <div className="switcher"></div>
-//     );
-//   }
-// }
-
-export default Switcher;

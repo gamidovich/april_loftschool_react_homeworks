7f48a0b113ff64d5681250fd4030895d04b68bac
Исаев Алексей Витальевич <anxieter@yandex.ru>
2018-04-27 12:22:35 +0300

    homework_3 done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,35 +6,49 @@ import Title from '../Title';
 import './App.css';
 
 class App extends Component {
-
   state = {
     step: 1,
     firstName: '',
     lastName: '',
     email: '',
     cardNumber: '',
-  }
+  };
 
   handleChangeForm = (a, b) => {
     const stateKeys = Object.keys(this.state);
     stateKeys.map(keyName => {
-      if (keyName === a) { this.setState({ [keyName]: b }) };
+      if (keyName === a) {
+        this.setState({ [keyName]: b });
+      }
       return null;
-    })
-  }
+    });
+  };
 
   renderForm = () => {
     const { step } = this.state;
     if (step === 1) {
-      return <PersonalForm firstName={this.state.firstName} lastName={this.state.lastName} email={this.state.email} onChangeForm={this.handleChangeForm} />
+      return (
+        <PersonalForm
+          firstName={this.state.firstName}
+          lastName={this.state.lastName}
+          email={this.state.email}
+          onChangeForm={this.handleChangeForm}
+        />
+      );
     } else if (step === 2) {
-      return <CardForm cardNumber={this.state.cardNumber} onChangeForm={this.handleChangeForm} onChangeTimeOver={this.handleChangeTimeOver} />
+      return (
+        <CardForm
+          cardNumber={this.state.cardNumber}
+          onChangeForm={this.handleChangeForm}
+          onChangeTimeOver={this.handleChangeTimeOver}
+        />
+      );
     } else if (step === 3) {
       return <p data-test="congratulations">Поздравляем!</p>;
     }
   };
 
-  handleTabClick = (step) => {
+  handleTabClick = step => {
     this.setState({ step });
   };
 
@@ -57,16 +71,14 @@ class App extends Component {
       //   } else if (state.firstName !== '' || state.lastName !== '' || state.email === '' || !state.email.includes('@')) {
       //     return true;
       //   }
-
     } else if (step === 2) {
       if (cardNumber.length === 16) return true;
-
-    } else if (step !== 1 | 2) return false;
-  }
+    } else if ((step !== 1) | 2) return false;
+  };
 
   handleClickNextForm = () => {
     this.setState(({ step }) => ({ step: step + 1 }));
-  }
+  };
 
   render() {
     let isDisabled = !this.isFormCommitable(); // так как должно быть по умолчанию false, а ф-ция вернула true
@@ -74,9 +86,30 @@ class App extends Component {
     return (
       <div className="container">
         <div className="tab-panel">
-          <Step number={1} isClickable={step > 1} isSelected={step === 1} onClick={this.handleTabClick}>Personal information</Step>
-          <Step number={2} isClickable={step > 2} isSelected={step === 2} onClick={this.handleTabClick}>Card information</Step>
-          <Step number={3} isClickable={false} isSelected={step === 3} onClick={this.handleTabClick}>Finish</Step>
+          <Step
+            number={1}
+            isClickable={step > 1}
+            isSelected={step === 1}
+            onClick={this.handleTabClick}
+          >
+            Personal information
+          </Step>
+          <Step
+            number={2}
+            isClickable={step > 2}
+            isSelected={step === 2}
+            onClick={this.handleTabClick}
+          >
+            Card information
+          </Step>
+          <Step
+            number={3}
+            isClickable={false}
+            isSelected={step === 3}
+            onClick={this.handleTabClick}
+          >
+            Finish
+          </Step>
         </div>
         <div className="form-content">
           <div className="" data-test="personal-form">
@@ -85,15 +118,17 @@ class App extends Component {
           </div>
         </div>
         <div className="button-panel">
-          <button className="button-next"
+          <button
+            className="button-next"
             type="button"
             onClick={this.handleClickNextForm}
-            disabled={isDisabled}>Next
+            disabled={isDisabled}
+          >
+            Next
           </button>
         </div>
       </div>
     );
   }
-
 }
 export default App;
--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,23 +2,19 @@ import React, { Component } from 'react';
 import './CardForm.css';
 
 class CardForm extends Component {
-
-  handleChangeForm = (evt) => {
+  handleChangeForm = evt => {
     return this.props.onChangeForm(evt.target.name, evt.target.value);
   };
 
-  componentWillUnmount = () => { };
+  componentWillUnmount = () => {};
 
   render() {
     return (
-      <div className="card-form">
+      <div className="card-form" data-test="card-form">
         <input type="text" name="cardNumber" onChange={this.handleChangeForm} />
       </div>
     );
-  };
+  }
 }
 
 export default CardForm;
-
-
-// change={this.onChangeForm}
